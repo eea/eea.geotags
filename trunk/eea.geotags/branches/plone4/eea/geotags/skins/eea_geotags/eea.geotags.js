a9a6ef18cf112d30a9872a8ad0131d82006244bd
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-09-19 12:30:43 +0000

    Refs #8527; Fixed jslint violation

--- trunk/eea.geotags/branches/plone4/eea/geotags/skins/eea_geotags/eea.geotags.js
+++ trunk/eea.geotags/branches/plone4/eea/geotags/skins/eea_geotags/eea.geotags.js
@@ -1356,7 +1356,7 @@ jQuery.fn.geopreview = function(settings){
       zoom: 3,
       navigationControl: true,
       navigationControlOptions: {
-        style: google.maps.NavigationControlStyle.ZOOM_PAN,
+        style: google.maps.NavigationControlStyle.ZOOM_PAN
 //        position: google.maps.ControlPosition.RIGHT // this generated a javascript error in IE
       },
       mapTypeControl: true,
