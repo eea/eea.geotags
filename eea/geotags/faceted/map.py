d755c9e94eac183b363eb7f33ffdae01adae24bc
ichim-david <ichim.david@gmail.com>
2012-06-08 08:44:02 +0000

    Refs #7339; got rid of the features hack and correctly display on map the points if there are more than one location set per object

--- eea/geotags/faceted/map.py
+++ eea/geotags/faceted/map.py
@@ -14,26 +14,24 @@ class MapView(BrowserView):
         """ Return geotags information found on brains
         """
         res = []
-
         props = getToolByName(self.context, 'portal_properties').site_properties
         for brain in brains:
             if brain.geotags:
                 # we only need the features items
-                feature = json.loads(brain.geotags).get('features')
-                location = feature[0]['properties']['description']
-                feature[0]['properties']['description'] = urllib.quote(
-                                    location.encode('utf-8'))
-                feature[0].update({"itemDescription":
-                                    urllib.quote(brain.Description)})
-                feature[0].update({"itemUrl": brain.getURL()})
-                feature[0].update({"itemTitle":
-                                    urllib.quote(brain.Title)})
-                start_date = brain.start.strftime(props.localLongTimeFormat)
-                end_date = brain.end.strftime(props.localLongTimeFormat)
-                feature[0].update({"itemDate": '%s to %s' % (
+                features = json.loads(brain.geotags).get('features')
+                for feature in features:
+                    location = feature['properties']['description']
+                    feature['properties']['description'] = urllib.quote(
+                                        location.encode('utf-8'))
+                    feature.update({"itemDescription":
+                                        urllib.quote(brain.Description)})
+                    feature.update({"itemUrl": brain.getURL()})
+                    feature.update({"itemTitle":
+                                        urllib.quote(brain.Title)})
+                    start_date = brain.start.strftime(props.localLongTimeFormat)
+                    end_date = brain.end.strftime(props.localLongTimeFormat)
+                    feature.update({"itemDate": '%s to %s' % (
                                                         start_date, end_date)})
-                feature = json.dumps(feature)
-                # hack as we only need the objects within the list
-                res.append(feature[1: -1])
-
+                    feature = json.dumps(feature)
+                    res.append(feature)
         return res
