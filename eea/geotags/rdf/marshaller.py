2ba6a6505be6f0659734a14eb7d86c55fec2ea1b
tiberich <tiberiuichim@users.noreply.github.com>
2012-10-03 14:14:54 +0000

    Refs #7409; added rdfmarshaler adapter for geotags field

---
+++ eea/geotags/rdf/marshaller.py
@@ -0,0 +1,65 @@
+""" RDF Marshaller module for geotags """
+from eea.rdfmarshaller.marshaller import ATField2Surf
+from eea.rdfmarshaller.interfaces import IATField2Surf, ISurfSession
+from eea.geotags.field.location import GeotagsFieldMixin
+from zope.component import adapts, getAdapter
+from zope.interface import implements
+from eea.geotags.interfaces import IGeoTags
+
+import surf
+
+class GeotagsField2Surf(ATField2Surf):
+    """Adapter to express geotags field with RDF using Surf."""
+    implements(IATField2Surf)
+    adapts(GeotagsFieldMixin, ISurfSession)
+
+    def value(self, context):
+        """ Value of geotags field """
+                
+        # create a GeoPoint Class
+        GeoPoint = self.session.get_class(surf.ns.GEO.Point)
+        GeoLat = self.session.get_class(surf.ns.GEO.Lat)
+        GeoLong = self.session.get_class(surf.ns.GEO.Long)
+        
+        value = self.field.getAccessor(context)()
+        
+        geo = getAdapter(context, IGeoTags)
+
+        output = []
+        i = 0
+        for point in geo.getPoints():
+            rdfp = self.session.get_resource("geotag%s" % i, GeoPoint)
+            rdfp[surf.ns.GEO['lat']] = GeoLat(point[0])
+            rdfp[surf.ns.GEO['long']] = GeoLong(point[1])
+            rdfp[surf.ns.RDFS['label']] = 'Rome'
+
+            rdfp.save()
+            output.append(rdfp)
+            i += 1
+
+        #this is like example 2 in #3425
+        #output.append((rdfp, None, surf.ns.GEO))   
+
+        return output
+
+"""desired output is:
+<document:Document 
+rdf:about="http://www.eea.europa.eu/data-and-maps/daviz/learn-more/prepare-data">
+...
+<dct:spatial>
+    <geo:Point>
+     <rdfs:label>Rome</rdfs:label>
+     <geo:lat>41.901514</geo:lat>
+     <geo:long>12.460774</geo:long>
+    </geo:Point>
+ </dct:spatial>
+<dct:spatial>
+    <geo:Point>
+     <rdfs:label>Bucharest</rdfs:label>
+     <geo:lat>44.437711</geo:lat>
+     <geo:long>26.097367</geo:long>
+    </geo:Point>
+ </dct:spatial>
+...
+</document:Document>
+"""
