40bc98b0a881d169f8c2ab7c8d5bd892895527f6
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2014-04-04 13:17:13 +0000

    Refs #18559; remove featureClass parameter if it's falsy since leaving it empty doesn't return any results like it happens when using the advanced search of geonames.org

--- trunk/eea.geotags/branches/feature_geo_filters/eea/geotags/json/__init__.py
+++ trunk/eea.geotags/branches/feature_geo_filters/eea/geotags/json/__init__.py
@@ -199,7 +199,7 @@ class GeoNamesJsonProvider(object):
         """
         query = kwargs.copy()
         query['featureClass'] = ['H', 'T']
-        return self.search(sort = True, **query)
+        return self.search(sort=True, **query)
 
     def search(self, sort=False, **kwargs):
         """ Search using geonames webservice
@@ -208,8 +208,11 @@ class GeoNamesJsonProvider(object):
             'type': 'FeatureCollection',
             'features': []
         }
-
         query = kwargs.copy()
+        # remove featureClass as a way to say search for all classes if the
+        # value is falsy
+        if not query['featureClass']:
+            del(query['featureClass'])
         query.setdefault('lang', 'en')
         query.setdefault('username', self.username)
         query = urllib.urlencode(query, doseq=1)
