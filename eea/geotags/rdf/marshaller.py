27dab97f66cea64ac36d1d59bdd824a5317b40e5
Irina Botez <irina.botez@eaudeweb.ro>
2018-04-24 15:37:14 +0300

    [refs #91504] Added dexterity adapter for location field

--- eea/geotags/rdf/marshaller.py
+++ eea/geotags/rdf/marshaller.py
@@ -2,59 +2,34 @@
 """ RDF Marshaller module for geotags """
 
 from eea.geotags.field.location import GeotagsFieldMixin
+from eea.geotags.behavior.geotags import GeoTag
 from eea.geotags.interfaces import IGeoTags
 from eea.geotags.rdf.country_groups import COUNTRY_GROUPS
+from eea.geotags.storage.interfaces import IGeoTagged
 from eea.rdfmarshaller.archetypes.fields import ATField2Surf
 from eea.rdfmarshaller.archetypes.interfaces import IATField2Surf
+from eea.rdfmarshaller.dexterity.fields import DXField2Surf
+from eea.rdfmarshaller.dexterity.interfaces import IDXField2Surf
 from eea.rdfmarshaller.interfaces import ISurfSession
 from zope.component import adapts, getAdapter
 from zope.interface import implements, Interface
+from zope.schema import Field
 
-import surf
 import rdflib
+import surf
 
 
-class GeotagsField2Surf(ATField2Surf):
-    """Adapter to express geotags field with RDF using Surf."""
-    implements(IATField2Surf)
-    adapts(GeotagsFieldMixin, Interface, ISurfSession)
+class GeotagsField2Surf(object):
 
     prefix = "dcterms"
     name = "spatial"
 
+    def get_location(self):
+        raise NotImplementedError('Subclassess must implement this!')
+        return set(GeoTag(self.context).location.split('\n'))
+
     def value(self):
-        """desired RDF output is like:
 
-        <document:Document
-        rdf:about="
-        http://www.eea.europa.eu/data-and-maps/daviz/learn-more/prepare-data">
-        ...
-        <dct:spatial>
-            <geo:SpatialThing>
-            <rdfs:label>Rome (Latium, Italy)</rdfs:label>
-            <dcterms:title>Rome (Latium, Italy)</dcterms:title>
-            <rdfs:comment>Latium, Italy</rdfs:comment>
-            <dcterms:type>capital of a political entity</dcterms:type>
-            <geo:lat>41.901514</geo:lat>
-            <geo:long>12.460774</geo:long>
-            <owl:sameAs rdf:resource="http://sws.geonames.org/3169070/">
-            </geo:SpatialThing>
-        </dct:spatial>
-        <dct:spatial>
-            <geo:SpatialThing>
-            <rdfs:label>Bucharest (București, Romania)</rdfs:label>
-            <dcterms:title>Bucharest (București, Romania)</dcterms:title>
-            <rdfs:comment>Bucureşti, Romania</rdfs:comment>
-            <dcterms:type>capital of a political entity</dcterms:type>
-            <geo:lat>44.437711</geo:lat>
-            <geo:long>26.097367</geo:long>
-            <owl:sameAs rdf:resource="http://sws.geonames.org/683506/">
-            </geo:SpatialThing>
-        </dct:spatial>
-        ...
-        </document:Document>
-
-        """
         # create a GeoPoint Class
         SpatialThing = self.session.get_class(surf.ns.GEO.SpatialThing)
 
@@ -105,7 +80,7 @@ class GeotagsField2Surf(ATField2Surf):
             other = feature['properties'].get('other', {})
             if other.has_key('geonameId'):
                 geonamesURI = 'http://sws.geonames.org/%s/' % (
-                            str(feature['properties']['other']['geonameId']))
+                    str(feature['properties']['other']['geonameId']))
                 rdfp[surf.ns.OWL['sameAs']] = rdflib.URIRef(geonamesURI)
             rdfp.update()
             output.append(rdfp)
@@ -113,7 +88,7 @@ class GeotagsField2Surf(ATField2Surf):
 
         # 85617 add country groups to rdf output
         found_groups = []
-        location = set(self.context.location)
+        location = self.get_location()
         correct_country_names = cty_names.values()
         for k, v in country_groups.items():
             differences = set(v).difference(location)
@@ -135,7 +110,8 @@ class GeotagsField2Surf(ATField2Surf):
             rdfp[surf.ns.DCTERMS['title']] = full_title
             rdfp[surf.ns.RDFS['label']] = full_title
             rdfp[surf.ns.RDFS['comment']] = title + ': ' + \
-                                    ', '.join(country_groups[(label, title)])
+                                            ', '.join(
+                                                country_groups[(label, title)])
             rdfp[surf.ns.DCTERMS['type']] = 'countries_group'
             rdfp[surf.ns.SKOS['notation']] = label
             uri = 'http://rdfdata.eionet.europa.eu/eea/countries/%s' % label
@@ -145,3 +121,20 @@ class GeotagsField2Surf(ATField2Surf):
             i += 1
 
         return output
+
+
+class GeotagsField2SurfDX(GeotagsField2Surf, DXField2Surf):
+    implements(IDXField2Surf)
+    adapts(Field, IGeoTagged, ISurfSession)
+
+    def get_location(self):
+        return set(GeoTag(self.context).location.split('\n'))
+
+
+class GeotagsField2SurfAT(GeotagsField2Surf, ATField2Surf):
+    """Adapter to express geotags field with RDF using Surf."""
+    implements(IATField2Surf)
+    adapts(GeotagsFieldMixin, Interface, ISurfSession)
+
+    def get_location(self):
+        return set(self.context.location.split('\n'))
